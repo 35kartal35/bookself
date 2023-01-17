6eeca9f4d3a454cfcf328fdd3f4a07c26738a008
35kartal35 <mozdemir3545@gmail.com>
2023-01-17 21:18:25 +0300

    week1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React,{useEffect} from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import HomePage from "./pages/HomePages";
+import { useDispatch } from "react-redux";
+import actionTypes from "./redux/actions/actionTypes";
+import api from "./api/api";
+import urls from "./api/urls";
+
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    dispatch({type:actionTypes.bookaction.GET_BOOKS_START})
+    api.get(urls.books)
+    .then((res)=>{
+      dispatch({type:actionTypes.bookaction.GET_BOOKS_SUCCESS,payload:res.data})
+    })
+    .catch((err)=>{
+      dispatch({type:actionTypes.bookaction.GET_BOOKS_FAİL,payload:"serverda bir hata oldu"})
+    })
+  },[])
+    return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
